b8fbdee181c9c2f1a25cd4ed87035e881cba5a02
KIIT <KIIT@KISHAN>
2026-04-27 17:49:03 +0530

    Add project files.

---
+++ DemoApp.Tests/Test1.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DemoApp;
+
+namespace DemoApp.Tests
+{
+    [TestClass]
+    public class Test1
+    {
+        [TestMethod]
+        public void Multiply_Test()
+        {
+            Calculator calc = new Calculator();
+            Assert.AreEqual(12, calc.Multiply(2, 6));
+        }
+
+        [TestMethod]
+        public void Add_Test()
+        {
+            Calculator calc = new Calculator();
+            Assert.AreEqual(8, calc.Add(5, 3));
+        }
+
+        [TestMethod]
+        public void Subtract_Test()
+        {
+            Calculator calc = new Calculator();
+            Assert.AreEqual(6, calc.Subtract(10, 4));
+        }
+
+        [TestMethod]
+        public void Divide_Test()
+        {
+            Calculator calc = new Calculator();
+            Assert.AreEqual(4, calc.Divide(8, 2));
+        }
+
+        [TestMethod]
+        public void Divide_By_Zero_Test()
+        {
+            Calculator calc = new Calculator();
+
+            try
+            {
+                calc.Divide(5, 0);
+                Assert.Fail("Expected exception was not thrown");
+            }
+            catch (DivideByZeroException)
+            {
+                Assert.IsTrue(true);
+            }
+        }
+    }
+}
---
+++ DemoApp/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace DemoApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calc = new Calculator();
+
+            Console.WriteLine("TDD Calculator Demo\n");
+            Console.WriteLine("Multiply: " + calc.Multiply(2, 6));
+            Console.WriteLine("Add: " + calc.Add(5, 3));
+            Console.WriteLine("Subtract: " + calc.Subtract(10, 4));
+            Console.WriteLine("Divide: " + calc.Divide(8, 2));
+        }
+    }
+
+    public class Calculator
+    {
+        public int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+
+        public int Divide(int a, int b)
+        {
+            if (b == 0)
+                throw new DivideByZeroException("Cannot divide by zero");
+
+            return a / b;
+        }
+    }
+}
